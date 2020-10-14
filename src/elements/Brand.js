54e76283c3aa0a8ef1cdfc381fdc651e2254e81f
Balbaal <iqbal.mi20@gmail.com>
2020-10-15 03:29:33 +0800

    add: brand component

--- src/elements/Brand.js
+++ src/elements/Brand.js
@@ -3,7 +3,7 @@ import React from "react";
 const Brand = (props) => {
   return (
     <img
-      src={props.img}
+      src={props.src}
       style={props.style}
       className={["", props.className].join(" ")}
     />
--- src/parts/ColumnFirst.js
+++ src/parts/ColumnFirst.js
@@ -6,7 +6,7 @@ import { Brand } from "elements";
 const ColumnFirst = () => {
   return (
     <div className="bg-light shadow vh-100">
-      <Brand />
+      <Brand style={{ width: 150 }} src="/images/pokemon_logo.png" />
     </div>
   );
 };
